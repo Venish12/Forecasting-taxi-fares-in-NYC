5f221d5abf1a38f8567800e718f028690064a0e8
venish korat <venishkorat@venishs-MacBook-Pro.local>
2026-02-07 12:11:01 -0800

    Dockerize API

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 
 class TaxiFareRequest(BaseModel):
-    pickup_datetime: str = Field(..., description="Example: 2015-01-27 13:08:24 UTC")
+    pickup_datetime: str = Field(..., description="Example: 2015-01-27 13:08:24 UTC", example="2015-01-27 13:08:24 UTC")
 
     pickup_longitude: float = Field(..., ge=-75, le=-72)
     dropoff_longitude: float = Field(..., ge=-75, le=-72)
